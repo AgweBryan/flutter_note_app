cc075f058df144fff6338e8ac3bedf4151dd3c5c
Agwe Bryan <agwedinobryan@gmail.com>
2022-09-11 21:36:18 +0100

    work on add note screen

--- lib/views/screens/addNote/add_note_screen.dart
+++ lib/views/screens/addNote/add_note_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/utils/colors.dart';
+import 'package:flutter_note_app/utils/custom_text.dart';
+import 'package:intl/intl.dart';
 
 class AddNoteScreen extends StatelessWidget {
   const AddNoteScreen({Key? key}) : super(key: key);
@@ -6,31 +9,82 @@ class AddNoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _appBar(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Note Title",
+      appBar: _appBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  child: CustomText(
+                    isTitle: true,
+                    fontSize: 16,
+                    text: DateFormat.yMMMd().format(
+                      DateTime.now(),
+                    ),
+                  ),
                 ),
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+                SizedBox(
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.folder,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 5),
+                        child: DropdownButtonHideUnderline(
+                          child: DropdownButton(
+                            value: "Note",
+                            icon: Icon(
+                              Icons.keyboard_arrow_down_rounded,
+                            ),
+                            items: ["Note", "School", "shopping"]
+                                .map((String item) {
+                              return DropdownMenuItem(
+                                value: item,
+                                child: CustomText(
+                                  text: item,
+                                  fontSize: 16,
+                                  isTitle: true,
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              // todo: change value
+                              print(newValue);
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Note Title",
               ),
-              Expanded(
-                  child: TextField(
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: TextField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
                     border: InputBorder.none, hintText: "your note..."),
-              ))
-            ],
-          ),
-        ));
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   _appBar() {
